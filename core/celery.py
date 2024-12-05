d38bb12a8b728844875eb9643e8beb531ff63040
anasazamov <anasazamov55@gmail.com>
2024-12-05 13:13:32 +0500

    remove commit

--- core/celery.py
+++ core/celery.py
@@ -2,16 +2,12 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# Django settings modulini ko‘rsatish
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-# Celery ilovasini yaratish
 app = Celery("core")
 
-# Django settings-dan konfiguratsiyalarni yuklash
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# Berilgan vazifalarni avtomatik kashf qilish
 app.autodiscover_tasks()
 
 # @app.task(bind=True)
